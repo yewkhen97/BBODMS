2671506e29f0baf51aeedf69325859b476ff53c2
yewkhen <yewkhen97@yahoo.com>
2020-11-14 01:51:26 +0800

    update 14Nov

--- BlockChain/Controller/ViewBlock/BlockRoute.py
+++ BlockChain/Controller/ViewBlock/BlockRoute.py
@@ -1,11 +1,18 @@
 from flask import render_template,  Blueprint, request, jsonify
-from BlockChain.Model.ViewBlockModel.ViewBlockModel import get_chain, register_peer, get_node, sync_new_block,set_session
+from BlockChain.Model.ViewBlockModel.ViewBlockModel import (get_chain, register_peer, get_node,
+                                                            sync_new_block,set_session,
+                                                            save_broadcast_node, is_conflict, resolve_conflict)
 
 BlockRoute = Blueprint('BlockRoute', __name__)
 @BlockRoute.route("/")
 @BlockRoute.route("/home")
 def home():
     return render_template('ViewBlock/home.html')
+@BlockRoute.route("/chain", methods=['GET'])
+def retreive_chain():
+    local_chain = get_chain()
+    dict_chain = [block.__dict__.copy() for block in local_chain]
+    return jsonify(dict_chain), 200
 
 @BlockRoute.route("/block_<int:block_id>/details")
 def block_details(block_id):
@@ -62,6 +69,7 @@ def get_all_node():
 
 @BlockRoute.route('/broadcast_block', methods=['POST'])
 def broadcast_block():
+    print("broadcast block is called")
     localblock = get_chain()
     value = request.get_json()
     if not value:
@@ -71,19 +79,32 @@ def broadcast_block():
         response = {"message": "block not found"}
         return jsonify(response), 400
     block = value['block']
+    print("Broadcast Block:",block)
     if block['index'] == localblock[-1].index+1:
         if sync_new_block(block):
             response = { "message": "Block added" }
             return jsonify(response), 201
         else:
             response = {"message": "Block declined"}
-            return jsonify(response), 500
+            return jsonify(response), 409
         
     elif block['index'] > localblock[-1].index:
-        pass
+        response ={"message" : "Block is differ with the local blockchain"}
+        is_conflict()
+        return jsonify(response), 200
     else:
         response = { "message" : "blockchain is smaller than local blockchain" }
         return jsonify(response), 409
+    
+@BlockRoute.route("/consensus", methods=['POST'] )
+def consensus():
+    replace = resolve_conflict()
+    if replace:
+        response = {"message": "blockchain is replaced"}
+    else:
+        response = {"message": "local blockchain is remaining"}
+
+    return jsonify(response), 200
 
 @BlockRoute.route("/get_ip")
 def get_ip():
@@ -95,3 +116,22 @@ def show_current_ip():
     set_session(current_node)
     response = {"message": "set node"}
     return jsonify(response), 200
+
+
+@BlockRoute.route('/broadcast_node', methods=['POST'])
+def broadcast_node():
+    value = request.get_json()
+    if not value:
+        response = {"message": "Value not found"}
+        return jsonify(response), 400
+    if 'nodes' not in value:
+        response = {"message": "nodes not found"}
+        return jsonify(response), 400
+    success = save_broadcast_node(value['nodes'])
+    if success:
+        response = {"message": "Node added"}
+        return jsonify(response), 201
+    else:
+        response = {"message": "Node declined"}
+        return jsonify(response), 500
+
--- BlockChain/Model/ManageDonationModel/DonationModel.py
+++ BlockChain/Model/ManageDonationModel/DonationModel.py
@@ -2,6 +2,8 @@ from BlockChain import db
 from BlockChain.models import Donation
 from BlockChain.blockchain import *
 from flask_login import current_user
+from flask import session
+
 
 def set_donation(form):
     timezone = pytz.timezone('Asia/Kuala_Lumpur')
@@ -12,28 +14,52 @@ def set_donation(form):
     db.session.commit()
     return True
 
+
 def get_donation_list():
         result=Donation.query.filter_by(pic=current_user)
         return result
+
 
 def get_pending_List():
     list=Donation.query.filter_by(approval_status="pending")
     return list
 
+
 def get_details(id):
     result=Donation.query.get_or_404(id)
     return result
 
+
 def set_approve_status(form, donation):
     donation.approval_status = form.approval_status.data
     db.session.commit()
-    retrieve_confirmed_donation()
 
 
 def retrieve_confirmed_donation():
     new_donation = Donation.query.filter_by(approval_status='Approved').first()
-    new_mine = BlockChain()
-    new_mine.mine(new_donation)
-    new_donation.approval_status = "Added"
+    new_mine = BlockChain(session['current_node'])
+    print("current session in donation model:",session['current_node'] )
+    new_mine.chain_retrive()
+    print(new_mine.get_chain())
+    if new_mine.mine(new_donation):
+        new_donation.approval_status = "Added"
+    else:
+        print("some thing wrong when adding new block")
+        new_donation.approval_status = "Approved"
     db.session.commit()
 
+
+def mine_new_block():
+    donation_list = Donation.query.filter_by(approval_status='Approved')
+    new_mine = BlockChain(session['current_node'])
+    new_mine.chain_retrive()
+    for donation in donation_list:
+        if new_mine.resolve_conflict:
+            return False
+        if new_mine.mine(donation):
+            donation.approval_status = "Added"
+        else:
+            print("some thing wrong when adding new block")
+            donation.approval_status = "Approved"
+    db.session.commit()
+    return True
